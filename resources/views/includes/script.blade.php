7b607e5718e64ab3d35733d0cabc20dda9a984d4
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2023-03-18 16:59:16 +0700

    Konversi Home Page

---
+++ resources/views/includes/script.blade.php
@@ -0,0 +1,7 @@
+<script src="{{ asset('templates/vendor/jquery/jquery.slim.min.js') }}"></script>
+<script src="{{ asset('templates/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+    AOS.init();
+</script>
+<script src="{{ asset('templates/script/navbar-scroll.js') }}"></script>
