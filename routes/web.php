c01b31635f121f3db96be5d8d166e6a08af6175e
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2023-04-23 00:08:29 +0700

    integrasi modul detail

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,8 @@ use App\Http\Controllers\DashboardTransactionController;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/categories', [CategoryController::class, 'index'])->name('categories');
 Route::get('/categories/{id}', [CategoryController::class, 'detail'])->name('categories.detail');
-Route::get('/details/{id}', [DetailController::class, 'detail'])->name('detail');
+Route::get('/details/{id}', [DetailController::class, 'index'])->name('detail');
+Route::post('/details/{id}', [DetailController::class, 'add'])->name('detail-add');
 Route::get('/cart', [CartController::class, 'index'])->name('cart');
 Route::get('/success', [CartController::class, 'success'])->name('success');
 
